b215887e8448a6781ce837d11fbc41c94f8ad70e
Ivan Uresti <ivan.uresti@gmail.com>
2025-03-10 20:52:15 -0600

    Añadir campos a Alumno

--- src/Alumno.java
+++ src/Alumno.java
@@ -1,2 +1,21 @@
 public class Alumno {
+
+    private String name;
+    private int generation;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGeneration() {
+        return generation;
+    }
+
+    public void setGeneration(int generation) {
+        this.generation = generation;
+    }
 }
